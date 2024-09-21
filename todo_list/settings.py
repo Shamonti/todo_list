4b9dd103c6e797731407b34d19b6b2621592cd04
Shamonti <shamonti.haque98@gmail.com>
2024-09-21 18:56:46 +0600

    Setup the project

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l6az(wv9c)$4oh16o#g_6kubdt4z2_k(j&2!3u6%%lyz&@j7ql"
+SECRET_KEY = "django-insecure-x=8ce#&%ndx-b%+%3pjir9t0-=_bm4!&s8fe06kn-s*yfn1d)a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "base.apps.BaseConfig",
 ]
 
 MIDDLEWARE = [
